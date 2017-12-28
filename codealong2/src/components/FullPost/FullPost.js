1ad7fdacac27286b75edb97243eb20488d3094e8
Monis Rizvi <monisriz@gmail.com>
2017-12-27 18:11:46 -0600

    codealong

--- codealong2/src/components/FullPost/FullPost.js
+++ codealong2/src/components/FullPost/FullPost.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './FullPost.css';
-
+// import axios from '../../axios';
 class FullPost extends Component {
 
   state = {
@@ -11,7 +11,7 @@ class FullPost extends Component {
   componentDidUpdate () {
     if (this.props.id) {
       if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-        axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        axios.get('/posts/' + this.props.id)
         .then(response => {
           this.setState({
             loadedPost: response.data
@@ -23,7 +23,7 @@ class FullPost extends Component {
   };
 
   deletePostHandler = () => {
-    axios.delete('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
+    axios.delete('/posts/' + this.props.id)
       .then(response => {
         console.log(response);
       })
