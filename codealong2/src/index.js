e4dcf2be840bb44a263794119b1b697ff93f3b70
Monis Rizvi <monisriz@gmail.com>
2017-12-29 10:14:02 -0600

    codealong

--- codealong2/src/index.js
+++ codealong2/src/index.js
@@ -5,10 +5,22 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 axios.interceptors.request.use(request => {
   console.log(request);
   //can also edit the request
   return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log(response);
+  return response;
 }, error => {
   console.log(error);
   return Promise.reject(error);
